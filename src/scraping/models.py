25047d83fbb472aec2724d6d687a546a2d7a27be
ovvladimir <ov.vladimir@mail.ru>
2020-06-25 16:30:21 +1000

    django

---
+++ src/scraping/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название населенного пункта', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+    s = models.Model
+
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+        
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
