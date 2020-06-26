f9d0ef66ee4f80b27f314ebc1bc8465209cbcc59
ovvladimir <ov.vladimir@mail.ru>
2020-06-26 18:54:38 +1000

    django

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, \
+    DeleteView
 
+from .forms import FindForm, VForm
 from .models import Vacancy
 
 
 def home_view(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    # qs = Vacancy.objects.all()
+    # return render(request, 'scraping/home.html', {'object_list': qs})
     # print(request.GET)
-    # form = FindForm()
 
-    # return render(request, 'scraping/home.html', {'form': form})
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
