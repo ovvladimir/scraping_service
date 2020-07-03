d9cd542f588b8412a116f0fdd6417e085765a057
ovvladimir <ov.vladimir@mail.ru>
2020-07-03 17:13:08 +1000

    django

--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.MyUser'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
