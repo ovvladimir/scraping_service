4bbc03f8f343ad4ab48a6e349c59857561d283ac
ovvladimir <ov.vladimir@mail.ru>
2020-07-09 18:50:46 +1000

    django

--- src/scraping_service/settings/production.py
+++ src/scraping_service/settings/production.py
@@ -22,6 +22,7 @@ DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -35,7 +36,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["serv-scrap.herokuapp.com"]
+ALLOWED_HOSTS = ["job-finder-my.herokuapp.com"]
 
 
 # Application definition
